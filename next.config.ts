748842af3a77ea07cbd300840d4b56f8ed64f776
feyi-tech <jinminetics@gmail.com>
2025-08-25 20:12:39 +0100

    build export config added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  distDir: "dist",   // 👈 this controls where the build files go
+  distDir: "build",   // 👈 this controls where the build files go
   output: "export", 
 };
 
